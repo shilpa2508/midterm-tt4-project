467f0770dc728b3eb9c010d25cf439b4da31b73a
Shilpa Shilpa <150201912+shilpa2508@users.noreply.github.com>
2025-02-25 14:44:48 -0500

    Product list page added

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -7,8 +7,7 @@ module.exports = {
   entry: {
     global: path.resolve(__dirname, "global.js"),
     index: path.resolve(__dirname, "index.js"),
-    // listProducts: "./list-products.js",
-    // addProducts: "./add-products.js",
+    listProducts: path.resolve(__dirname, "list-products.js"),
   },
   output: {
     path: path.resolve(__dirname, "dist"),
@@ -37,16 +36,16 @@ module.exports = {
       },
       {
         test: /\.(png|jpg|gif)$/,
-        type: "asset/resource", // Para copiar arquivos de imagem
+        type: "asset/resource",
         generator: {
-          filename: 'assets/images/[name][hash][ext][query]', // Colocando as imagens dentro de assets/images/
+          filename: 'assets/images/[name][hash][ext][query]',
         },
       },
       {
         test: /\.(woff(2)?|eot|ttf)$/,
-        type: "asset/resource", // Para copiar arquivos de fontes
+        type: "asset/resource",
         generator: {
-          filename: 'assets/fonts/[name][hash][ext][query]', // Colocando as fontes dentro de assets/fonts/
+          filename: 'assets/fonts/[name][hash][ext][query]',
         },
       },      
     ],
@@ -60,16 +59,11 @@ module.exports = {
       chunks: ["global", "index"],
       filename: "index.html",
     }),
-    // new HtmlWebpackPlugin({
-    //   template: "./list-products.html",
-    //   chunks: ["listProducts", "global"],
-    //   filename: "list-products.html",
-    // }),
-    // new HtmlWebpackPlugin({
-    //   template: "./add-products.html",
-    //   chunks: ["addProducts", "global"],
-    //   filename: "add-products.html",
-    // }),
+    new HtmlWebpackPlugin({
+      template: "./list-products.html",
+      chunks: ["listProducts", "global"],
+      filename: "list-products.html",
+    }),
   ],
   optimization: {
     minimize: true,
